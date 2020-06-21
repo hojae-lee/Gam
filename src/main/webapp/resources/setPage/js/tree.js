03f401318eb1c86139cba143cce325dfa2fa41b3
Choi MinSung <zizi27177@naver.com>
2020-06-21 18:14:28 +0900

    -

--- src/main/webapp/resources/setPage/js/tree.js
+++ src/main/webapp/resources/setPage/js/tree.js
@@ -1,3 +1,22 @@
+GAM.addOne = [
+	'<tr><td>'
+	, '<i class="fa fa-plus addLvOne"></i>'
+	, '</td></tr>'
+].join('');
+
+GAM.addTwo = [
+	'<tr><td>'
+	, '&emsp;&emsp;&emsp;'
+	, '<i class="fa fa-plus addLvTwo"></i>'
+	, '</td></tr>'
+].join('');
+
+GAM.addThree = [
+	'<tr><td>'
+	, '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;'
+	, '<i class="fa fa-plus addLvThree"></i>'
+	, '</td></tr>'
+].join('');
 
 GAM.row1 = [
 	'<tr><td>'
@@ -7,51 +26,34 @@ GAM.row1 = [
 	, '&emsp;'
 	, '<i class="fa fa-check"></i> <i class="fa fa-times delRow"></i>'
 	, '</td></tr>'
-	, '<tr><td class="addTwo">'
-	, '&emsp;&emsp;&emsp;'
-	, '<i class="fa fa-plus addLvTwo"></i>'
-	, '</td></tr>'
-	, '<tr>'
-	, '<td><i class="fa fa-plus addLvOne"></i>'
-	, '</td></tr>'
 ].join('');
 
 GAM.row2 = [
-	'<tr><td>'
+	'<tr><td id="Two">'
 	, '&emsp;&emsp;&emsp;'
 	, '<i class="fas fa-angle-right"></i>'
 	, '&emsp;'
 	, '<input type="text">'
 	, '&emsp;'
 	, '<i class="fa fa-check"></i> <i class="fa fa-times delRow"></i>' 
-	, '</td></tr>'
-	, '<tr><td class="addThree">'
-	, '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;'
-	, '<i class="fa fa-plus addLvThree"></i>'
-	, '</td></tr>'
-	, '<tr><td>'
-	, '&emsp;&emsp;&emsp;'
-	, '<i class="fa fa-plus addLvTwo"></i>'
 	, '</td></tr>'
 ].join('');
 
 GAM.row3 = [
-	'<tr><td>'
+	'<tr><td id="Three">'
 	, '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;'
 	, '<i class="fas fa-angle-right"></i>'
 	, '&emsp;'
 	, '<input type="text">'
 	, '&emsp;'
 	, '<i class="fa fa-check"></i> <i class="fa fa-times delRow"></i>' 
-	, '</td></tr>'
-	, '<tr><td>'
-	, '&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;&emsp;'
-	, '<i class="fa fa-plus addLvThree"></i>'
 	, '</td></tr>'
 ].join('');
 
 $(document).on("click", ".addLvOne", function() {
 	$(".table").append(GAM.row1);
+	$(".table").append(GAM.addTwo);
+	$(".table").append(GAM.addOne);
 	$(this).closest("tr").remove();
 });
 
@@ -59,6 +61,8 @@ $(document).on("click", ".addLvTwo", function() {
 	var tmpTwo = $(this).parent();
 	
 	$(tmpTwo).append(GAM.row2);
+	$(tmpTwo).append(GAM.addThree);
+	$(tmpTwo).append(GAM.addTwo);
 	$(this).closest(".addLvTwo").remove();
 });
 
@@ -66,9 +70,19 @@ $(document).on("click", ".addLvThree", function() {
 	var tmpThree = $(this).parent();
 	
 	$(tmpThree).append(GAM.row3);
+	$(tmpThree).append(GAM.addThree);
 	$(this).closest(".addLvThree").remove();
 });
 
 $(document).on("click", ".delRow", function() {
-	$(this).closest("tr").remove();
+	var saveLv = $(this).closest("td").attr("id");
+	var tmp = $(this).parent().parent().siblings();
+	
+	if (saveLv == "Three") {
+		$(this).closest("td").remove();
+	} else if (saveLv == "Two") {
+		tmp[0].remove();
+		$(this).closest("td").remove();
+	}
+	
 });
